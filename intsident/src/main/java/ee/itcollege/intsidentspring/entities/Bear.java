21e0dccd323b3269efbf9cf5ca76a67c8513c4b1
Indrek Kõue <indrek.koue@gmail.com>
2011-11-24 18:39:47 +0200

    Spring raamistik

---
+++ intsident/src/main/java/ee/itcollege/intsidentspring/entities/Bear.java
@@ -0,0 +1,11 @@
+package ee.itcollege.intsidentspring.entities;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public class Bear {
+}
